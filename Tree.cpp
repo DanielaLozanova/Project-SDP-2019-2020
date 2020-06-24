193b13c43a8104a6fab5aa3be685b417d70090a3
DanielaLozanova <32839272+DanielaLozanova@users.noreply.github.com>
2020-06-24 19:12:20 +0300

    Add files via upload

--- Tree.cpp
+++ Tree.cpp
@@ -1,4 +1,6 @@
 #include "Tree.h"
+#include<stack>
+#include<queue>
 int Tree::id = 1;
 Node *newNode(int key)
 {
@@ -6,7 +8,8 @@ Node *newNode(int key)
 	temp->key = key;
 	return temp;
 }
-void Tree::clear(Node *root) {
+void Tree::clear(Node *&root) 
+{
 	if (!root)
 		return;
 
@@ -25,14 +28,15 @@ Tree::~Tree()
 	clear(root);
 }
 
-void Tree::addLeaf(Node * root,int boss)
+void Tree::addLeaf(Node * root, int boss)
 {
 	if (!root)
 	{
 		return;
 	}
 
-	if (root->key == boss) {
+	if (root->key == boss)
+	{
 		++id;
 		root->child.push_back(newNode(id));
 		return;
@@ -41,7 +45,7 @@ void Tree::addLeaf(Node * root,int boss)
 
 	for (size_t i = 0; i < root->child.size(); ++i)
 	{
-			addLeaf(root->child[i], boss);
+		addLeaf(root->child[i], boss);
 	}
 
 }
@@ -51,6 +55,46 @@ void Tree::add(int boss)
 	addLeaf(root, boss);
 }
 
+void Tree::delBoss(int boss)
+{
+	if (root->key == boss)
+	{
+		cerr << "Cannot dismiss the Boss.\n";
+		return;
+	}
+	Node* bossPtr = findBoss(boss);
+	if (bossPtr == nullptr)
+	{
+		cerr << "Such employee does not exist!"<<endl;
+		return;
+	}
+	pushUp(bossPtr);
+}
+Node* Tree::findBoss(int boss)
+{
+	queue<Node*> q;
+	q.push(root);
+
+	while (!q.empty())
+	{
+		Node* temp = q.front();
+		q.pop();
+
+		for (size_t i = 0; i < temp->child.size(); ++i)
+		{
+			if (temp->child[i]->key == boss)
+			{
+				return temp->child[i];
+			}
+			else
+			{
+				q.push(temp->child[i]);
+			}
+		}
+	}
+
+	return nullptr;
+}
 void Tree::removeLeaf(Node * root, int boss)
 {
 	if (!root)
@@ -72,12 +116,83 @@ void Tree::removeLeaf(Node * root, int boss)
 	}
 }
 
+Node* Tree::findNewBoss(Node * root)
+{
+	int height=0;
+	int max=0;
+	Node* ptrMax = nullptr;
+	for (size_t i = 0; i < root->child.size(); ++i)
+	{
+		height = DFS(root->child[i]);
+		if (height > max)
+		{
+			max = height;
+			ptrMax = root->child[i];
+		}
+	}
+	return ptrMax;
+}
+
+int Tree::DFS(Node * root)
+{
+	int curr = -1;
+	int max = 0;
+	stack<Node*> st;
+	st.push(root);
+	Node* temp = nullptr;
+	while (!st.empty())
+	{
+		temp = st.top();
+		st.pop();
+		++curr;
+		if (curr > max)
+		{
+			max = curr;
+		}
+		if (temp->child.empty()) --curr;
+		else
+		{
+			for (size_t i = 0; i < temp->child.size(); ++i)
+			{
+				st.push(temp->child[i]);
+			}
+		}
+	}
+	return max;
+}
+
+void Tree::pushUp(Node * root)
+{
+	Node* newBoss = findNewBoss(root);
+	vector<Node*> restore = newBoss->child;
+
+	root->key = newBoss->key;
+
+	for (int i = 0; i < root->child.size(); ++i)
+	{
+		if (root->child[i] == newBoss) 
+		{
+			root->child.erase(root->child.begin() + i);
+			break;
+		}
+	}
+
+	for (int i = 0; i < restore.size(); ++i)
+	{
+		root->child.push_back(restore[i]);
+	}
+}
+
+
 void Tree::remove(int boss)
 {
-	if (root->key == boss) {
+	if (root->key == boss)
+	{
 		cerr << "Cannot dismiss the Boss.\n";
 		return;
 	}
 
 	removeLeaf(root, boss);
 }
+
+
--- Tree.h
+++ Tree.h
@@ -15,14 +15,26 @@ private:
 	Node* root;
 	static int id;
 
-	void clear(Node *root);
+	void clear(Node *&root);
 	void addLeaf(Node *root, int boss);
+	void removeLeaf(Node* root, int boss);
+	Node* findNewBoss(Node* root);
+	int DFS(Node* root);
+	void pushUp(Node* root);
+	Node* findBoss(int boss);
 public:
 	Tree();
 	~Tree();
-	
+
 	void add(int boss);
-	void removeLeaf(Node* root, int boss);
+	void delBoss(int boss);
 	void remove(int boss);
+	
 };
 
+/*struct idDFS
+{
+	int id;
+	int dfsH;
+	idDFS(int id = -1, int dfsH = -1);
+};*/
